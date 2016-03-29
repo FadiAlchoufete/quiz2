9edf18ea6a8fbc546893c169cbf656965e7c6e0b
Fadi Alchoufete <fadi.alchoufete@gmail.com>
2016-03-28 20:53:14 -0400

    Bug fixes. Styling changes. Added AJAX call for getting random strings.

--- template/quiz2.js
+++ template/quiz2.js
@@ -5,8 +5,9 @@
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
 	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$click     = $('.click');
+	$submit    = $('.submit');
+	$timeout   = $('.timeout');
 
 	$mouseover.on('mouseover', function() {
 		$this = $(this);
@@ -14,26 +15,77 @@
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	$click.click(function() {
+		$(this).html('Peace Out!')
+		$(this).fadeOut(1500);
 		return false;
 	});
 
 	$submit.on('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$(this).find('input').each(function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
+	$(document).on('ready', function() {
+		setTimeout(function() {
 			$timeout.fadeIn('slow');
 		}, 1000);
+
+		changeData(false);
+	});
+
+	var isCalled = false;
+	var options;
+	var keep = "";
+
+	function changeData(change = true) {
+		if (change == true) {
+			var r = Math.floor(Math.random() * (options.length));
+
+			keep = options[r];
+
+			$("#btn-gt").html("Change It");
+			$("#btn-k").css("display", "inline");
+		}else {
+			keep = document.cookie;
+		}
+
+		if (keep != "") {
+			$("#gt-response").html("You got: <i>" + keep + "</i>");
+			$("#gt-response").css("display", "block");
+		}
+	}
+
+	$("#btn-gt").click(function() {
+		if (isCalled == false) {
+			$.ajax({
+				url: 'http://www.mattbowytz.com/simple_api.json',
+				data: 'data=quizData',
+				dataType: 'json',
+				statusCode: {
+					200: function( response ) {
+						options = response.data;
+						changeData();
+						isCalled = true;
+					},
+					500: function( text ) {
+						alert("An error occurred getting the data");
+					}
+				}
+			});
+		}else {
+			changeData();
+		}
+	});
+
+	$("#btn-k").click(function() {
+		document.cookie = keep;
+		alert("Your selected string has been saved");
 	});
 
 })(jQuery);
